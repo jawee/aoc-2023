7e5ed2ec2e9c16c4b13528743bf83cd0fe79e25c
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-02 06:06:57 +0000

    day2 done

--- day2/day2.go
+++ day2/day2.go
@@ -5,40 +5,99 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
     pwd, _ := os.Getwd()
     // file, err := os.Open(pwd + "/day2/atest.txt")
-    // file, err := os.Open(pwd + "/day2/btest.txt")
-    file, err := os.Open(pwd + "/day2/atest.txt")
+    file, err := os.Open(pwd + "/day2/input.txt")
 
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
     defer file.Close()
-    res := a(file)
-    // res := b(file)
+    // res := a(file)
+    res := b(file)
     fmt.Printf("%d\n", res)
 }
 
-func getGameIdIfPossible(s string) int {
-    fmt.Printf("%s\n", s)
-    split := strings.Split(s, ":")
-    fmt.Printf("%s\n", split[1])
-    gameSplit := strings.Split(split[1], ";")
+func getGameIdIfPossible(input string) int {
+    maxRed := 12
+    maxGreen := 13
+    maxBlue := 14
 
+    split := strings.Split(input, ":")
 
-    return 0;
+    gameIdSplit := strings.Split(split[0], " ")
+    gameId, _ := strconv.Atoi(gameIdSplit[1])
+
+    for _, frames := range strings.Split(strings.Trim(split[1], " "), ";") {
+
+        frameValues := strings.Split(frames, ",") 
+        for _, frameValue := range frameValues {
+            frameSplit := strings.Split(strings.Trim(frameValue, " "), " ")
+            no,_ := strconv.Atoi(frameSplit[0])
+            if frameSplit[1] == "blue" && no > maxBlue {
+                return 0
+            }
+            if frameSplit[1] == "red" && no > maxRed {
+                return 0
+            }
+            if frameSplit[1] == "green" && no > maxGreen {
+                return 0
+            }
+        }
+    }
+
+    return gameId;
 }
+
+func getPower(input string) int {
+    maxRed := 0 
+    maxGreen := 0
+    maxBlue := 0
+
+    split := strings.Split(input, ":")
+    for _, frames := range strings.Split(strings.Trim(split[1], " "), ";") {
+
+        frameValues := strings.Split(frames, ",") 
+        for _, frameValue := range frameValues {
+            frameSplit := strings.Split(strings.Trim(frameValue, " "), " ")
+            no,_ := strconv.Atoi(frameSplit[0])
+            if frameSplit[1] == "blue" && no > maxBlue {
+                maxBlue = no
+            }
+            if frameSplit[1] == "red" && no > maxRed {
+                maxRed = no
+            }
+            if frameSplit[1] == "green" && no > maxGreen {
+                maxGreen = no
+            }
+        }
+    }
+
+    return maxRed * maxGreen * maxBlue;
+}
+
+func b(file io.Reader) int {
+    scanner := bufio.NewScanner(file)
+    sum := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        sum += getPower(line)
+    }
+
+    return sum
+}
+
 func a(file io.Reader) int {
     scanner := bufio.NewScanner(file)
     sum := 0
     for scanner.Scan() {
         line := scanner.Text()
-        // fmt.Printf("%s\n", line)
         sum += getGameIdIfPossible(line)
     }
 
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -4,5 +4,31 @@ import (
 	"testing"
 )
 
-func Test1(t *testing.T) {
+func Test2A1(t *testing.T) {
+    input := "Game 1: 3 blue, 4 red"
+
+    res := getGameIdIfPossible(input)
+
+    if res != 1 {
+        t.Fatalf("Expected 1, got %d\n", res)
+    }
+}
+func Test2A2(t *testing.T) {
+    input := "Game 1: 3 blue, 13 red"
+
+    res := getGameIdIfPossible(input)
+
+    if res != 0 {
+        t.Fatalf("Expected 0, got %d\n", res)
+    }
+}
+
+func Test2B1(t *testing.T) {
+    input := "Game 1: 3 blue, 4 red, 2 green"
+
+    res := getPower(input)
+
+    if res != 24 {
+        t.Fatalf("Expected 12, got %d\n", res)
+    }
 }
