46ca74ef5e5c68970d8bf538d73f738dd82b3d8b
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-02 06:29:31 +0100

    began day2

--- day2/day2.go
+++ day2/day2.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
     pwd, _ := os.Getwd()
     // file, err := os.Open(pwd + "/day2/atest.txt")
     // file, err := os.Open(pwd + "/day2/btest.txt")
-    file, err := os.Open(pwd + "/day2/input.txt")
+    file, err := os.Open(pwd + "/day2/atest.txt")
 
     if err != nil {
         fmt.Println(err)
@@ -23,12 +24,22 @@ func main() {
     fmt.Printf("%d\n", res)
 }
 
+func getGameIdIfPossible(s string) int {
+    fmt.Printf("%s\n", s)
+    split := strings.Split(s, ":")
+    fmt.Printf("%s\n", split[1])
+    gameSplit := strings.Split(split[1], ";")
+
+
+    return 0;
+}
 func a(file io.Reader) int {
     scanner := bufio.NewScanner(file)
     sum := 0
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Printf("%s\n", line)
+        // fmt.Printf("%s\n", line)
+        sum += getGameIdIfPossible(line)
     }
 
     return sum
