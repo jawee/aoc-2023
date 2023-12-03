329b9929711872651da03a463698ca684d765354
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-03 07:34:28 +0000

    day3a

--- day3/day3.go
+++ day3/day3.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -36,11 +38,102 @@ func b(file io.Reader) int {
 
 func a(file io.Reader) int {
     scanner := bufio.NewScanner(file)
-    sum := 0
+    matrix := [][]string{}
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Printf("%s\n", line)
+        arr := strings.Split(line, "")
+        matrix = append(matrix, arr)
+    }
+
+    sum := calculateSumA(matrix)
+
+    return sum
+}
+
+func isDigit(s string) bool {
+    if s== "0" || s == "1" || s == "2" || s == "3"|| s == "4"|| s == "5"|| s == "6"|| s == "7" || s == "8" || s == "9" {
+        return true
+    }
+    return false
+}
+
+func calculateSumA(matrix [][]string) int {
+    sum := 0 
+
+    for y := 0; y < len(matrix); y++ {
+        for x := 0; x < len(matrix[y]); x++ {
+            xv := matrix[y][x]
+            number := ""
+            foundPartNo := false
+            for isDigit(xv) {
+                number += xv
+                if isPartNumber(y,x,matrix) {
+                    foundPartNo = true
+                }
+                x++
+                if x >= len(matrix[y]) {
+                    break
+                }
+                xv = matrix[y][x]
+            }
+            if foundPartNo {
+                val, _ := strconv.Atoi(number)
+                sum += val
+            }
+        }
     }
 
     return sum
+}
+
+func isPartNumber(y, x int, matrix [][]string) bool {
+    //top left
+    if y-1 >= 0 && x-1 >= 0 {
+        if matrix[y-1][x-1] != "." && !isDigit(matrix[y-1][x-1]) {
+            return true
+        }
+    }
+    //top
+    if y-1 >= 0 {
+        if matrix[y-1][x] != "." && !isDigit(matrix[y-1][x]) {
+            return true
+        }
+    }
+    //top right
+    if y-1 >= 0 && x+1 < len(matrix[y]) {
+        if matrix[y-1][x+1] != "." && !isDigit(matrix[y-1][x+1]) {
+            return true
+        }
+    }
+    //right
+    if x+1 < len(matrix[y]) {
+        if matrix[y][x+1] != "." && !isDigit(matrix[y][x+1]) {
+            return true
+        }
+    }
+    //bottom right
+    if y+1 < len(matrix) && x+1 < len(matrix[y]) {
+        if matrix[y+1][x+1] != "." && !isDigit(matrix[y+1][x+1]) {
+            return true
+        }
+    }
+    //bottom
+    if y+1 < len(matrix) {
+        if matrix[y+1][x] != "." && !isDigit(matrix[y+1][x]) {
+            return true
+        }
+    }
+    //bottom left
+    if y+1 < len(matrix) && x-1 >= 0 {
+        if matrix[y+1][x-1] != "." && !isDigit(matrix[y+1][x-1]) {
+            return true
+        }
+    }
+    //left
+    if x-1 >= 0 {
+        if matrix[y][x-1] != "." && !isDigit(matrix[y][x-1]) {
+            return true
+        }
+    }
+    return false
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -1,9 +1,140 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
+func createMatrix(lines []string) [][]string {
+    matrix := [][]string{}
+    for _, v := range lines {
+        matrix = append(matrix, strings.Split(v, ""))
+    }
+
+    return matrix
+} 
+
+
 
 func Test1(t *testing.T) {
+    testCases := []struct {
+        lines    []string
+        expected int
+    }{
+        {
+            []string{
+                "..1..",
+                ".#...",
+            },
+            1,
+        },
+        {
+            []string{
+                "..1..",
+                ".....",
+            },
+            0,
+        },
+        {
+            []string{
+                "..1..",
+                "..#..",
+            },
+            1,
+        },
+        {
+            []string{
+                "..1..",
+                "...#.",
+            },
+            1,
+        },
+        {
+            []string{
+                "..1#.",
+                ".....",
+            },
+            1,
+        },
+        {
+            []string{
+                ".#1..",
+                ".....",
+            },
+            1,
+        },
+        {
+            []string{
+                ".#...",
+                "..1..",
+            },
+            1,
+        },
+        {
+            []string{
+                "..#..",
+                "..1..",
+            },
+            1,
+        },
+        {
+            []string{
+                "...#.",
+                "..1..",
+            },
+            1,
+        },
+        {
+            []string{
+                "..1#.",
+                "..1..",
+            },
+            2,
+        },
+        {
+            lines: []string{
+                "..123",
+                ".#...",
+            },
+            expected: 123,
+        },
+        {
+            lines: []string{
+                "1....",
+                ".#...",
+            },
+            expected: 1,
+        },
+        {
+            lines: []string{
+                "123..",
+                "...#.",
+            },
+            expected: 123,
+        },
+        {
+            lines: []string{
+                "1...1",
+                "1#.#1",
+            },
+            expected: 4,
+        },
+        {
+            lines: []string {
+                "206#13..",
+                "........",
+            },
+            expected: 219,
+        },
+    }
+    for i, v := range testCases {
+        fmt.Printf("TestCase %d\n", i)
+        matrix := createMatrix(v.lines)
+        sum := calculateSumA(matrix)
+
+        if sum != v.expected {
+            t.Errorf("Expected '%d', got '%d'\n", v.expected, sum)
+        }
+    }
 }
