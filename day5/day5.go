1c88d101a04ae327b1a916d33980ac6b06aef8b4
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-05 06:08:40 +0000

    day5a

--- day5/day5.go
+++ day5/day5.go
@@ -11,9 +11,9 @@ import (
 
 func main() {
     pwd, _ := os.Getwd()
-    file, err := os.Open(pwd + "/day5/atest.txt")
+    // file, err := os.Open(pwd + "/day5/atest.txt")
     // file, err := os.Open(pwd + "/day5/btest.txt")
-    // file, err := os.Open(pwd + "/day5/input.txt")
+    file, err := os.Open(pwd + "/day5/input.txt")
 
     if err != nil {
         fmt.Println(err)
@@ -29,18 +29,93 @@ func b(file io.Reader) int {
     scanner := bufio.NewScanner(file)
     sum := 0
     for scanner.Scan() {
-        line := scanner.Text()
+        // line := scanner.Text()
     }
 
     return sum
 }
 
+type mapLine struct {
+    dest int
+    src int
+    length int
+}
+
+func newMapLine(s string) mapLine {
+    vals := strings.Split(s, " ")
+    dest, _ := strconv.Atoi(vals[0])
+    src, _ := strconv.Atoi(vals[1])
+    length, _ := strconv.Atoi(vals[2])
+    return mapLine {
+        dest: dest,
+        src: src,
+        length: length,
+    }
+}
+
 func a(file io.Reader) int {
     scanner := bufio.NewScanner(file)
     sum := 0
-    for scanner.Scan() {
-        line := scanner.Text()
+    scanner.Scan() 
+    line := scanner.Text()
+    seeds := strings.Split(strings.Trim(strings.Split(line, ":")[1], " "), " ")
+    seedsArr := []int{}
+    for _, v := range seeds {
+        n, _ := strconv.Atoi(v)
+        seedsArr = append(seedsArr, n)
     }
 
+    // fmt.Printf("Seeds: %v\n", seedsArr);
+    scanner.Scan(); //header
+    for scanner.Scan() { //empty line
+        // fmt.Printf("CurrSeedsArr: %v\n", seedsArr)
+        nextDest := []int{}
+        line = scanner.Text()
+        // fmt.Printf("Header: %s\n", line)
+        scanner.Scan()
+        line = scanner.Text()
+        destArr := []mapLine{}
+        for line != "" {
+            m := newMapLine(line)
+            destArr = append(destArr, m)
+            // fmt.Printf("%+v\n", m)
+            scanner.Scan()
+            line = scanner.Text()
+        }
+
+        // fmt.Printf("%+v\n", destArr)
+        for _, v := range seedsArr {
+            found := false
+            for _, r := range destArr {
+                if r.src <= v && v <= r.src+r.length {
+                    dest := (v-r.src) + r.dest
+                    // fmt.Printf("%d matches %+v. New dest: %d\n", v, r, dest)
+                    nextDest = append(nextDest, dest)
+                    found = true
+                    break;
+                }
+            }
+            if !found {
+                // fmt.Printf("%d matches nothing\n", v)
+                nextDest = append(nextDest, v)
+            }
+        }
+
+        seedsArr = nextDest
+    }
+
+    sum = getMin(seedsArr)
+
     return sum
+}
+
+func getMin(values []int) int {
+    min := values[0]
+    for _, v := range values {
+        if (v < min) {
+            min = v
+        }
+    }
+
+    return min
 }
