ffeead775a1ec9c2dcddbeb58bd96afc846fb0c2
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-06 06:08:16 +0000

    day6b

--- day6/day6.go
+++ day6/day6.go
@@ -19,18 +19,48 @@ func main() {
         os.Exit(1)
     }
     defer file.Close()
-    res := a(file)
-    // res := b(file)
+    // res := a(file)
+    res := b(file)
     fmt.Printf("%d\n", res)
 }
 
 
 func b(file io.Reader) int {
     scanner := bufio.NewScanner(file)
-    sum := 0
-    for scanner.Scan() {
-        // line := scanner.Text()
+    sum := 1
+    scanner.Scan()
+    tmpTimes := strings.Split(strings.Split(scanner.Text(), ":")[1], " ")
+    scanner.Scan()
+    tmpDists := strings.Split(strings.Trim(strings.Split(scanner.Text(), ":")[1], " "), " ")
+
+    times := []string{}
+    for _, v := range tmpTimes {
+        if v == "" {
+            continue
+        }
+        times = append(times, v)
     }
+    dists := []string{}
+    for _, v := range tmpDists {
+        if v == "" {
+            continue
+        }
+        dists = append(dists, v)
+    }
+
+    time, _ := strconv.Atoi(strings.Join(times, ""))
+    dist, _ := strconv.Atoi(strings.Join(dists, ""))
+
+    timesToWin := 0
+    for j := 1; j < time; j++ {
+        timeToTravel := time - j
+        speed := j
+
+        if timeToTravel*speed > dist {
+            timesToWin++
+        }
+    }
+    sum = sum * timesToWin
 
     return sum
 }
