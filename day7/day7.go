d9268f45e60df26f73d736ebbfb7cde163cb7639
Andreas Olsson <olsson.andreas@gmail.com>
2023-12-09 05:52:58 +0000

    day7b give up

--- day7/day7.go
+++ day7/day7.go
@@ -14,25 +14,178 @@ func main() {
     pwd, _ := os.Getwd()
     // file, err := os.Open(pwd + "/day7/atest.txt")
     file, err := os.Open(pwd + "/day7/input.txt")
-
+    
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
     defer file.Close()
-    res := a(file)
-    // res := b(file)
+    // res := a(file)
+    res := b(file)
     fmt.Printf("%d\n", res)
 }
 
 func b(file io.Reader) int {
     scanner := bufio.NewScanner(file)
     sum := 0
+    hands := []hand{}
     for scanner.Scan() {
-        // line := scanner.Text()
+        line := scanner.Text()
+        split := strings.Split(line, " ")
+
+        bet, _ := strconv.Atoi(split[1])
+        cards := strings.Split(split[0], "")
+
+        hand := hand {
+            cards: cards,
+            bet: bet,
+        }
+
+        hands = append(hands, hand)
     }
 
+    slices.SortStableFunc(hands, func(a, b hand) int {
+        if isHigherRankB(a, b) {
+            return 1
+        }
+        return -1
+    })
+
+    for i := range hands {
+        sum += (i+1) * hands[i].bet
+    }
     return sum
+}
+
+func isHigherRankB(higher, lower hand) bool {
+    hType := getTypeB(higher)
+    lType := getTypeB(lower)
+    if hType < lType {
+        // fmt.Printf("%s < %s\n", hType, lType)
+        return true
+    }
+
+    if hType == lType {
+        for i := range higher.cards {
+            h := higher.cards[i]
+            l := lower.cards[i]
+            if ranksB[h] < ranksB[l] {
+                // fmt.Printf("%d < %d\n", ranksB[h], ranksB[l])
+                return true
+            }
+            if ranksB[h] > ranksB[l] {
+                // fmt.Printf("%d > %d\n", ranksB[h], ranksB[l])
+                return false
+            }
+        }
+    }
+
+    // fmt.Printf("%s > %s\n", hType, lType)
+    return false
+}
+
+func getTypeB(a hand) handType {
+    counts := map[string]int{}
+    for _, v := range a.cards {
+        counts[v]++
+    }
+
+    jokers := counts["J"]
+
+    keys := []string{}
+    for k := range counts {
+        keys = append(keys, k)
+    }
+
+    if len(keys) == 1 {
+        return Five
+    }
+
+    if len(keys) == 2 {
+        for _, k := range keys {
+            if k == "J" {
+                continue
+            }
+            if counts[k] == 4 {
+                if jokers == 1 {
+                    return Five
+                }
+                return Four
+            }
+            if counts[k] == 3 {
+                if jokers == 2 {
+                    return Five
+                }
+                if jokers == 1 {
+                    return Four
+                }
+                return FullHouse
+            }
+        }
+    }
+
+    if len(keys) == 3 {
+        for _, k := range keys {
+            if k == "J" {
+                continue
+            }
+            if counts[k] == 3 {
+                if jokers == 2 {
+                    return Five
+                }
+                if jokers == 1 {
+                    return Four
+                }
+                return Three
+            }
+            if counts[k] == 2 {
+                if jokers == 2 {
+                    return Four
+                }
+                if jokers == 1 {
+                    return FullHouse
+                }
+                return TwoPair
+            }
+        }
+    }
+    if len(keys) == 4 {
+        for _, k := range keys {
+            if k == "J" {
+                continue
+            }
+            if counts[k] == 2 {
+                if jokers == 3 {
+                    return Five
+                }
+                if jokers == 2 {
+                    return Four
+                }
+                if jokers == 1 {
+                    return Three
+                }
+                return OnePair
+            }
+        }
+    }
+
+    if jokers == 5 {
+        return Five
+    }
+    if jokers == 4 {
+        return Five
+    }
+    if jokers == 3 {
+        return Four
+    }
+    if jokers == 2 {
+        return Three
+    }
+    if jokers == 1 {
+        return OnePair
+    }
+
+    return HighCard
 }
 
 type hand struct {
@@ -40,7 +193,23 @@ type hand struct {
     bet int
 }
 
-var ranks = map[string]int {
+var ranksB = map[string]int {
+    "A": 1,
+    "K": 2,
+    "Q": 3,
+    "T": 5,
+    "9": 6,
+    "8": 7,
+    "7": 8,
+    "6": 9,
+    "5": 10,
+    "4": 11,
+    "3": 12,
+    "2": 13,
+    "J": 14,
+}
+
+var ranksA = map[string]int {
     "A": 1,
     "K": 2,
     "Q": 3,
@@ -100,11 +269,11 @@ func isHigherRank(higher, lower hand) bool {
         for i := range higher.cards {
             h := higher.cards[i]
             l := lower.cards[i]
-            if ranks[h] < ranks[l] {
+            if ranksA[h] < ranksA[l] {
                 // fmt.Printf("%d < %d\n", ranks[h], ranks[l])
                 return true
             }
-            if ranks[h] > ranks[l] {
+            if ranksA[h] > ranksA[l] {
                 // fmt.Printf("%d > %d\n", ranks[h], ranks[l])
                 return false
             }
--- day7/day7_test.go
+++ day7/day7_test.go
@@ -1,12 +1,256 @@
 package main
 
 import (
-	"fmt"
 	"slices"
 	"strings"
 	"testing"
 )
 
+func TestIsHigherHandB(t *testing.T) {
+    testCases := []struct {
+        a string
+        b string
+        exp bool
+    }{
+        {
+            a: "32T3K",
+            b: "KK677",
+            exp: false,
+        },
+        {
+            a: "T55J5",
+            b: "KK677",
+            exp: true,
+        },
+        {
+            a: "QQQJA",
+            b: "T55J5",
+            exp: true,
+        },
+        {
+            a: "KTJJT",
+            b: "QQQJA",
+            exp: true,
+        },
+        {
+            a: "22222",
+            b: "JJJJJ",
+            exp: true,
+        },
+        {
+            a: "22222",
+            b: "2222J",
+            exp: true,
+        },
+        {
+            a: "22222",
+            b: "J222J",
+            exp: true,
+        },
+        {
+            a: "22222",
+            b: "J2222",
+            exp: true,
+        },
+        {
+            a: "2345J",
+            b: "J345A",
+            exp: true,
+        },
+        {
+            a: "JJJJJ",
+            b: "AAAAA",
+            exp: false,
+        },
+        {
+            a: "AJJJJ",
+            b: "AAAAA",
+            exp: false,
+        },
+        {
+            a: "AAAAA",
+            b: "AAAAA",
+            exp: false,
+        },
+    }
+
+    for i, v := range testCases {
+        a := getHand(v.a)
+        b := getHand(v.b)
+        res := isHigherRankB(a, b)
+
+        if res != v.exp {
+            t.Fatalf("Case %d: Got '%v' expected '%v'\n", i, res, v.exp)
+        }
+    }
+}
+
+func TestGetHandTypeB(t *testing.T) {
+    testCases := []struct {
+        h string    
+        exp handType
+    }{
+        {
+            h: "AAAAA",
+            exp: Five,
+        },
+        {
+            h: "AA8AA",
+            exp: Four,
+        },
+        {
+            h: "23332",
+            exp: FullHouse,
+        },
+        {
+            h: "TTT98",
+            exp: Three,
+        },
+        {
+            h: "23432",
+            exp: TwoPair,
+        },
+        {
+            h: "A23A4",
+            exp: OnePair,
+        },
+        {
+            h: "23456",
+            exp: HighCard,
+        },
+        {
+            h: "KK677",
+            exp: TwoPair,
+        },
+        {
+            h: "KTJJT",
+            exp: Four,
+        },
+        {
+            h: "T55J5",
+            exp: Four,
+        },
+        {
+            h: "QQQJA",
+            exp: Four,
+        },
+        {
+            h: "AJJJJ",
+            exp: Five,
+        },
+        {
+            h: "JJJJJ",
+            exp: Five,
+        },
+        {
+            h: "32T3K",
+            exp: OnePair,
+        },
+        {
+            h: "KK677",
+            exp: TwoPair,
+        },
+        {
+            h: "T55J5",
+            exp: Four,
+        },
+        {
+            h: "KTJJT",
+            exp: Four,
+        },
+        {
+            h: "QQQJA",
+            exp: Four,
+        },
+        {
+            h: "J2233",
+            exp: FullHouse,
+        },
+        {
+            h: "JAABB",
+            exp: FullHouse,
+        },
+        {
+            h: "AABBJ",
+            exp: FullHouse,
+        },
+        {
+            h: "2345J",
+            exp: OnePair,
+        },
+        {
+            h: "AAAJJ",
+            exp: Five,
+        },
+        {
+            h: "4T48J",
+            exp: Three,
+        },
+        {
+            h: "Q2KJJ",
+            exp: Three,
+        },
+    }
+
+
+    for i, v := range testCases {
+        h := getHand(v.h)
+        res := getTypeB(h)
+
+        if res != v.exp {
+            t.Fatalf("Case %d '%s': Got '%s' expected '%s'\n", i, v.h, res, v.exp)
+        }
+    }
+}
+
+func TestSortB(t *testing.T) {
+    a:= hand {
+        cards: strings.Split("32T3K", ""),
+        bet: 765,
+    }
+
+    b := hand {
+        cards: strings.Split("T55J5", ""),
+        bet: 684,
+    }
+    c := hand {
+        cards: strings.Split("KK677", ""),
+        bet: 28,
+    }
+    d := hand {
+        cards: strings.Split("KTJJT", ""),
+        bet: 220,
+    }
+
+    e := hand {
+        cards: strings.Split("QQQJA", ""),
+        bet: 483,
+    }
+    hands := []hand{a, b, c, d, e}
+
+    slices.SortFunc(hands, func(a, b hand) int {
+        if isHigherRankB(a, b) {
+            return 1
+        }
+        return -1
+    })
+
+    if hands[0].bet != 765 {
+        t.Fatalf("Failed on 0\n")
+    }
+    if hands[1].bet != 28 {
+        t.Fatalf("Failed on 1\n")
+    }
+    if hands[2].bet != 684 {
+        t.Fatalf("Failed on 2\n")
+    }
+    if hands[3].bet != 483 {
+        t.Fatalf("Failed on 3\n")
+    }
+    if hands[4].bet != 220 {
+        t.Fatalf("Failed on 4\n")
+    }
+}
 
 func TestSort(t *testing.T) {
     a:= hand {
@@ -40,7 +284,6 @@ func TestSort(t *testing.T) {
         return -1
     })
 
-    fmt.Printf("%+v\n", hands)
     if hands[0].bet != 765 {
         t.Fatalf("Failed on 0\n")
     }
